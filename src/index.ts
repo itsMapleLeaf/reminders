5d0b8d5109be54309d3628e53f0865e92b6ae6aa
kingdaro <kingdaro@gmail.com>
2018-06-09 01:25:04 -0400

    add shitty-looking tray

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,38 @@
-import { app, BrowserWindow } from "electron"
+import { app, BrowserWindow, screen, Tray } from "electron"
 import { watch } from "fs"
 import { resolve } from "path"
 
 const devMode = process.argv.includes("--dev")
 
 let win: BrowserWindow | null
+let tray: Tray | null
 
 app.on("ready", () => {
-  win = new BrowserWindow()
+  const windowWidth = 600
+  const windowHeight = 400
+  const displayBounds = screen.getPrimaryDisplay().workArea
+
+  win = new BrowserWindow({
+    frame: false,
+    width: windowWidth,
+    height: windowHeight,
+    x: displayBounds.width - windowWidth - 10,
+    y: displayBounds.height - windowHeight - 10,
+  })
 
   win.loadFile(resolve(__dirname, "../assets/index.html"))
 
   win.on("closed", () => {
     win = null
   })
+
+  win.on("blur", () => {
+    if (win) win.hide()
+  })
+
+  tray = new Tray(resolve(__dirname, "../assets/icon.png"))
+
+  tray.on("click", () => win && win.show())
 
   if (devMode) {
     win.webContents.openDevTools()
