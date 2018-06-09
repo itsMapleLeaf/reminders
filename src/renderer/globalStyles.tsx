2944712fe6b853d7880a6516ea9d84293a0aaafc
kingdaro <kingdaro@gmail.com>
2018-06-09 02:48:15 -0400

    using `css` creates an extra unnecessary class name, so never mind

--- src/renderer/globalStyles.tsx
+++ src/renderer/globalStyles.tsx
@@ -1,36 +1,24 @@
-import { css, injectGlobal } from "react-emotion"
+import { injectGlobal } from "react-emotion"
 import { textColor, themeColor } from "./colors"
-
-// using a separate css declaration so prettier can format it
-const globalStyles = css`
-  *,
-  *::before,
-  *::after {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
-
-  :root {
-    background-color: ${themeColor};
-    color: ${textColor};
-    font: 18px Roboto, sans-serif;
-    cursor: default;
-  }
-
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6 {
-    font-family: "Roboto Condensed", sans-serif;
-    font-weight: 300;
-  }
-`
 
 export function applyGlobalStyles() {
   injectGlobal`
-    ${globalStyles};
+    *, *::before, *::after {
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+    }
+
+    :root {
+      background-color: ${themeColor};
+      color: ${textColor};
+      font: 18px Roboto, sans-serif;
+      cursor: default;
+    }
+
+    h1, h2, h3, h4, h5, h6 {
+      font-family: "Roboto Condensed", sans-serif;
+      font-weight: 300;
+    }
   `
 }
