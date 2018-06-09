0222c92551832af4e0812f8ba25d27f3fb4c5448
kingdaro <kingdaro@gmail.com>
2018-06-09 01:03:06 -0400

    add watch-reloading

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { app, BrowserWindow } from "electron"
+import { watch } from "fs"
 import { resolve } from "path"
+
+const devMode = process.argv.includes("--dev")
 
 let win: BrowserWindow | null
 
@@ -8,9 +11,16 @@ app.on("ready", () => {
 
   win.loadFile(resolve(__dirname, "../assets/index.html"))
 
-  win.webContents.openDevTools()
-
   win.on("closed", () => {
     win = null
   })
+
+  if (devMode) {
+    win.webContents.openDevTools()
+
+    const reloadWindow = win.webContents.reload.bind(win.webContents)
+
+    watch(resolve(__dirname, "../assets"), reloadWindow)
+    watch(resolve(__dirname, "./renderer"), reloadWindow)
+  }
 })
